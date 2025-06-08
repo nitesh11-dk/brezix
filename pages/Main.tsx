9093a70dea5c05d980d17451c5aa16594b7f2eed
Nitesh-DK <nittesh.dk@gmail.com>
2025-06-08 14:41:59 +0530

    serevies is disabled

--- pages/Main.tsx
+++ pages/Main.tsx
@@ -33,7 +33,7 @@ const Main = () => {
             <Navigation />
             <HeroSection />
             <AboutSection />
-            <Services />
+            {/* <Services /> */}
 
             <ProjectPreview />
             <Projects />
